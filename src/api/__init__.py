49f533ac8b4310bad2f1636c69c44a0c8a4e400e
juan montoya <piperivas@gmail.com>
2024-05-30 12:34:11 -0500

    Cambios para el nombre la descrión de la api de Swagger

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -6,7 +6,7 @@ from .activity import register_routes
 def create_app():
     app = Flask(__name__)
     CORS(app)  # Enable CORS for all routes and origins
-    api = Api(app, title="Calendar API", version="0.1.0")
+    api = Api(app, title="Calendario", version="0.1.0")
     register_routes(api)
 
     return app
